58d2f7ef5af8eff47d27d1cf27fa062af2d4d019
Carolina <krito_j_g@utp.edu.co>
2017-09-23 14:02:14 -0500

    Agrega algunas relaciones en los modelos, y modifica algunas migraciones de la bd

--- src/app/Models/Facturacion/factura_producto.php
+++ src/app/Models/Facturacion/factura_producto.php
@@ -4,7 +4,17 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class factura_producto extends Model
+class FacturaProducto extends Model
 {
     protected $table = 'factura_producto';
+
+    protected $fillable = ['id_factura', 'id_articulo', 'cantidad', 'precio_venta'];
+
+    public function factura() {
+      return $this->belongsTo('App\Facturacion\Factura');
+    }
+
+    // public function articulo() {
+    //   return $this->hasMany('App\Inventario\Articulo');
+    // }
 }
