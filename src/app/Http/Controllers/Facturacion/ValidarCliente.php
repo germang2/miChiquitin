9cbef587550d17b390c74369b8863412292f5f3e
andres felipe <pipe251296@gmail.com>
2017-10-15 14:36:09 -0500

    validación venta a vendedores

---
+++ src/app/Http/Controllers/Facturacion/ValidarCliente.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Facturacion;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Usuarios\Empleado;
+
+class ValidarCliente extends Controller
+{
+
+
+	public function validar($id_cliente, $id_vendedor){
+
+		$Empleado = Empleado::where("id_empleado", $id_cliente)->get();
+		if(sizeof($Empleado) > 0){
+			if(strcmp($Empleado[0]->cargo, "vendedor") == 0){
+				echo "El cliente es un vendedor.";
+				$Vendedor = Empleado::where("id_empleado", $id_vendedor)->get();
+				if(sizeof($Vendedor) > 0){
+					if(strcmp($Vendedor[0]->cargo, "administrador") == 0){
+						echo "\n El vendedor es un administrador, se puede ralizar la venta.";
+					}
+					else{
+						echo "No es un administrador, no se puede realizar la venta.";
+					}
+				}
+			}
+		}
+		else{
+			echo "No es un vendedor, se puede realizar la venta.";
+		}
+		#dd();
+		#dd($Empleado[0]->cargo);
+
+	}
+
+
+}
