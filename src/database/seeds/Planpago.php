795e81570b8fa0a307eb78c6299fb1093bd70a96
germang2 <germandg99@gmail.com>
2017-11-19 13:53:43 -0500

    Se borran controladores repetidos de inventario

--- src/database/seeds/Planpago.php
+++ src/database/seeds/Planpago.php
@@ -10,28 +10,24 @@ class Planpago extends Seeder
     public function run()
     {
         DB::table('plan_de_pagos')->insert([
-        	'id_plan_de_pago' => '1',
         	'nombre_plan' => 'Efectivo',
         	'cuotas' => '0',
         	'interes' => '0',
         	'forma_pago' => 'Efectivo'
         ]);
         DB::table('plan_de_pagos')->insert([
-        	'id_plan_de_pago' => '2',
         	'nombre_plan' => '1 Mes',
         	'cuotas' => '1',
         	'interes' => '0',
         	'forma_pago' => 'Efectivo'
         ]);
         DB::table('plan_de_pagos')->insert([
-        	'id_plan_de_pago' => '3',
         	'nombre_plan' => '3 Meses',
         	'cuotas' => '3',
         	'interes' => '0',
         	'forma_pago' => 'Efectivo'
         ]);
         DB::table('plan_de_pagos')->insert([
-        	'id_plan_de_pago' => '4',
         	'nombre_plan' => '6 Meses',
         	'cuotas' => '6',
         	'interes' => '0',
